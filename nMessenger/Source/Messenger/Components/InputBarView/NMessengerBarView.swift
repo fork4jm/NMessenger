aad1e816634f70900858e44586466360a8572364
dianhong.ge@jiamiantech.com <dianhong.ge@jiamiantech.com>
2018-05-21 03:20:19 +0800

    fix
    fix

--- nMessenger/Source/Messenger/Components/InputBarView/NMessengerBarView.swift
+++ nMessenger/Source/Messenger/Components/InputBarView/NMessengerBarView.swift
@@ -148,7 +148,7 @@ open class NMessengerBarView: InputBarView, UITextViewDelegate, CameraViewDelega
                 textInputViewHeight.constant = textInputViewHeightConst
                 textInputAreaViewHeight.constant = textInputViewHeightConst+10
                 _ = self.controller.sendText(self.textInputView.text,isIncomingMessage: false)
-//                self.textInputView.text = ""
+                self.textInputView.text = ""
                 return false
             }
         }
